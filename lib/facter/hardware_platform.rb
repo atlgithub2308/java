e2df669cc101cb9a6a53e8b0f017c48575800b3b
atlgithub2308 <89908586+atlgithub2308@users.noreply.github.com>
2021-09-15 11:44:52 +0800

    Update hardware_platform.rb

--- lib/facter/hardware_platform.rb
+++ lib/facter/hardware_platform.rb
@@ -1,4 +1,9 @@
 Facter.add('hardware_platform') do
+  
+  confine :kernel do |value|
+    value == "Linux"
+  end
+  
   setcode do
     Facter::Core::Execution.execute('/bin/uname --hardware-platform')
   end
